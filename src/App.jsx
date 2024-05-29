c79748f8277ffdabae6647eee9d7dd0bd15f19c4
unknown <gabriel.alejandro.paparello@gmail.com>
2024-05-29 12:00:50 -0300

    commmit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export const App = () => {
       <h2 className="text-3xl text-center my-10 ">Projects</h2>
       <Projects
         projectName={"Secret Santa"}
-        projectImg={"./src/assets/project1.png"}
+        projectImg={"../src/assets/project1.png"}
         ProjectDescription={"I made this project so i can play with my co-workers avoiding the waste of paper. For the logic just an array random sort and for the result i send user the emails with the rules they set, as a prompt the app needs participants cuanties,  names and emails"}
         ProjectTechnologies={"i used Tailwind css for the styling , react for the logic and for the UI, git as a version control, react router for routing , to avoid backend i use EmailJS to handle emails"}
         ProjectLink={'/InvisibleFriend'}
@@ -17,7 +17,7 @@ export const App = () => {
       />
       <Projects
         projectName={"Position Assignment"}
-        projectImg={"./src/assets/project12.png"}
+        projectImg={"../src/assets/project12.png"}
         ProjectDescription={''}
         ProjectTechnologies={''}
         ProjectLink={'/numberAsigner'}
@@ -25,11 +25,11 @@ export const App = () => {
       />
       <Projects
         projectName={"Portfolio for client"}
-        projectImg={"./src/assets/project3.png"}
+        projectImg={"../src/assets/project3.png"}
         ProjectLink={'https://gabrielacandio.netlify.app/'}
         ProjectDisabled={true}
       />
-      <Projects projectImg={"./src/assets/project4.png"}
+      <Projects projectImg={"../src/assets/project4.png"}
       ProjectLink={'/TicTacToe'}
       />
     </>
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { faJs } from "@fortawesome/free-brands-svg-icons/faJs";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-
 export const Projects = ({
   projectName,
   projectImg,
