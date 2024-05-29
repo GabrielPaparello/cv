e5d3ae89bc00761a0a8d7b8b0dca8a7e78ee8bf3
unknown <gabriel.alejandro.paparello@gmail.com>
2024-05-29 12:25:00 -0300

    fixedimgsforgood

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import hero from '../assets/hero.png'
 export const Header = () => {
   return (
       <div className='text-center relative text-white'>
-          <img src="../src/assets/hero.jpg" alt="foto hero de fondo" className='w-max-full ' />
+          <img src={hero} alt="foto hero de fondo" className='w-max-full ' />
           <h1 className='text-3xl absolute top-10 left-10 '>Gabriel</h1>
           <h2 className='text-2xl absolute m-2 top-16 left-10'>Front End Developer</h2>
 
