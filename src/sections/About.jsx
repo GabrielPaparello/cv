fe636eb1d8e505deba31ce03c565569e01f08e20
unknown <gabriel.alejandro.paparello@gmail.com>
2024-06-03 13:25:22 -0300

    asda

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -4,7 +4,7 @@ export const About = () => {
       <div className="text-white flex flex-col  md:flex-row  mt-10 text-start   ">
         <section className="w-[400px] md:w-[580px]">
           <h2 className="text-2xl md:text-3xl font-serif text-[#00df9a] m-2"> About Me</h2>
-          <p className="text-lg font-serif md:text-2xl mt-2 pb-4 ">
+          <p className="text-lg font-serif md:text-2xl mt-2 pb-2 ">
             Im  passionate on learning how things work and how they can be improved.
             
           </p>
@@ -14,8 +14,8 @@ export const About = () => {
             </p>
         </section>
         <section className="md:w-[400px] w-[360px]">
-          <h2 className="text-2xl mb-2 md:text-3xl text-[#00df9a]">Tecnologies</h2>
-          <p className="text-lg md:text-xl font-serif ">
+          <h2 className="text-2xl my-2 md:text-3xl text-[#00df9a]">Tecnologies</h2>
+          <p className="text-lg md:text-xl font-serif mb-1 ">
             Currently my stack is: <br />
           </p>
             <p className="font-bold text-md  font-sans text-[#d1ff00]  ">
