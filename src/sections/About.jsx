57bab3982622d23c4b5fac9aa2675fe1332bf802
unknown <gabriel.alejandro.paparello@gmail.com>
2024-06-03 13:23:30 -0300

    asda

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -4,16 +4,16 @@ export const About = () => {
       <div className="text-white flex flex-col  md:flex-row  mt-10 text-start   ">
         <section className="w-[400px] md:w-[580px]">
           <h2 className="text-2xl md:text-3xl font-serif text-[#00df9a] m-2"> About Me</h2>
-          <p className="text-lg font-serif md:text-2xl m-2 pb-4 ">
+          <p className="text-lg font-serif md:text-2xl mt-2 pb-4 ">
             Im  passionate on learning how things work and how they can be improved.
             
           </p>
-            <p className="mt-2 md:w-[400px] md:text-lg w-[300px] text-md">
+            <p className=" md:w-[400px] md:text-lg w-[300px] text-md">
             On the constant search for learning and improving my knowlegde, I consider myself a comunicative person and a responsabilities addict.
             
             </p>
         </section>
-        <section className="ml-2 md:w-[400px] w-[360px]">
+        <section className="md:w-[400px] w-[360px]">
           <h2 className="text-2xl mb-2 md:text-3xl text-[#00df9a]">Tecnologies</h2>
           <p className="text-lg md:text-xl font-serif ">
             Currently my stack is: <br />
